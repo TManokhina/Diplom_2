08ca05266269b5ec09925e7697868cc84f2d9dd5
Tmanokhina <tmanokhi@yandex.ru>
2024-11-22 02:42:40 +0300

    Create UserTokens

---
+++ src/main/java/site/nomoreparties/stellarburger/UserTokens.java
@@ -0,0 +1,18 @@
+package site.nomoreparties.stellarburger;
+
+public class UserTokens {
+
+    private String accessToken;
+    private String refreshToken;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+
+
+}

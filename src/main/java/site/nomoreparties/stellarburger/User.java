e1850dabdce91f4b2d2ab6c23dbe63714d4910d3
Tmanokhina <tmanokhi@yandex.ru>
2024-11-22 02:43:11 +0300

    Create User

---
+++ src/main/java/site/nomoreparties/stellarburger/User.java
@@ -0,0 +1,21 @@
+package site.nomoreparties.stellarburger;
+
+public class User {
+    private String email;
+    private String password;
+    private String name;
+
+    public User withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+    public User withPassword(String password){
+        this.password = password;
+        return this;
+    }
+    public User withName(String name){
+        this.name = name;
+        return this;
+    }
+
+}

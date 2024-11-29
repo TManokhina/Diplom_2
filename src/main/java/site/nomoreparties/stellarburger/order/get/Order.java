d2936f8c6bd09d213edd36b1959e3470c25d962b
Tmanokhina <tmanokhi@yandex.ru>
2024-11-30 00:45:44 +0300

    Commit forgotten files

---
+++ src/main/java/site/nomoreparties/stellarburger/order/get/Order.java
@@ -0,0 +1,68 @@
+
+package site.nomoreparties.stellarburger.order.get;
+
+import java.util.List;
+
+public class Order {
+
+    private String _id;
+
+    private String createdAt;
+
+    private List<String> ingredients;
+
+    private Long number;
+
+    private String status;
+
+    private String updatedAt;
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
